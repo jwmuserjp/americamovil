de051b1ec09d47bde73dd59d0f759c4180113740
Jorge Pérez <jorge.perez@jwmsolutions.com>
2013-03-05 16:24:59 -0600

    version inicial

---
+++ assets/www/app/app.js
@@ -0,0 +1,4 @@
+Lungo.ready(function(){
+
+	
+});
---
+++ assets/www/app/events.touch.js
@@ -0,0 +1,8 @@
+$$("[data-action=show_acerca]").tap(function(event) {
+	Lungo.Element.count("section#acerca nav.groupbar a:nth-child(3)",222);
+    setTimeout(function(){
+		//html.scroll-enabled body.app section#acerca.show nav.groupbar a
+		
+		console.debug("a",Lungo.dom("section#acerca nav.groupbar a"));
+	},5000);		
+});
