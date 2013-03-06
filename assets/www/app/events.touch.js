28fd56852f2be0ea2cbccb7142098deb62104a2d
Jorge Pérez <jorge.perez@jwmsolutions.com>
2013-03-05 18:35:14 -0600

    se corrigen acentos y se quita el data-count en la seccion "acerca de"

--- assets/www/app/events.touch.js
+++ assets/www/app/events.touch.js
@@ -1,8 +1,8 @@
 $$("[data-action=show_acerca]").tap(function(event) {
-	Lungo.Element.count("section#acerca nav.groupbar a:nth-child(3)",222);
-    setTimeout(function(){
+	//Lungo.Element.count("section#acerca nav.groupbar a:nth-child(3)",222);
+    //setTimeout(function(){
 		//html.scroll-enabled body.app section#acerca.show nav.groupbar a
 		
-		console.debug("a",Lungo.dom("section#acerca nav.groupbar a"));
-	},5000);		
+		//console.debug("a",Lungo.dom("section#acerca nav.groupbar a"));
+	//},5000);		
 });
